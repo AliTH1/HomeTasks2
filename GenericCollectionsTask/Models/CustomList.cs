8d3cbbe9a54776a271baa6c9636030245d6e2f82
Ali Taghizada <alitaqizade2@gmail.com>
2023-04-27 10:25:28 +0400

    first commit

---
+++ GenericCollectionsTask/Models/CustomList.cs
@@ -0,0 +1,77 @@
+﻿using GenericCollectionsTask.Utilities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollectionsTask.Models
+{
+    internal class CustomList<T>
+    {
+        public T[] arr = new T[0];
+
+
+        public void Add(T inputValue)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = inputValue;
+        }
+
+        public void Remove(T inputValue)
+        {
+            bool isExist = true;
+            T[] arrCopy = new T[0];
+            foreach (T item in arr)
+            {
+                if (EqualityComparer<T>.Default.Equals(inputValue, item))
+                {
+                    isExist = false;
+                    continue;
+                }
+                
+                Array.Resize(ref arrCopy, arrCopy.Length + 1);
+                arrCopy[arrCopy.Length - 1] = item;
+            }
+
+            if (isExist)
+            {
+                throw new NotFoundException("Not founded element");
+            }
+
+
+            arr = arrCopy;
+        }
+
+        public void Clear()
+        {
+            T[] arrCopy = new T[0];
+            arr = arrCopy;
+        }
+
+        public bool Contains(T inputValue)
+        {
+            foreach (T item in arr)
+            {
+                if (EqualityComparer<T>.Default.Equals(inputValue, item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        public int Count()
+        {
+            int count = 0;
+            foreach (T item in arr)
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ GenericCollectionsTask/Program.cs
@@ -0,0 +1,32 @@
+﻿using GenericCollectionsTask.Models;
+using GenericCollectionsTask.Utilities.Exceptions;
+
+CustomList<int> customList = new CustomList<int>();
+customList.Add(5);
+customList.Add(6);
+
+for (int i = 0; i < customList.arr.Length; i++)
+{
+    Console.WriteLine($"Item {i + 1} array: {customList.arr[i]}");
+}
+
+
+Console.WriteLine("Count: " + customList.Count());
+Console.WriteLine(customList.Contains(5));
+//customList.Clear();
+
+
+try
+{
+    customList.Remove(4);
+}
+catch (NotFoundException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+for (int i = 0; i < customList.arr.Length; i++)
+{
+    Console.WriteLine($"Item {i + 1} array: {customList.arr[i]}");
+}
