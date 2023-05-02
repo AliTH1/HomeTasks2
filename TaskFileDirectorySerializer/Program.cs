426ac3f4add0b05614dd9f3e9f29289c134c4ea5
Ali Taghizada <alitaqizade2@gmail.com>
2023-05-02 08:13:26 +0400

    added task

---
+++ TaskFileDirectorySerializer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+string path = @"C:\\Users\\alita\\source\\repos\\TaskFileDirectorySerializer\\";
+Directory.CreateDirectory(path + @"\\Models");
+Directory.CreateDirectory(path + @"\\Data");
+
+
+if (!File.Exists(path + @"\\Data\\jsonData.json"))
+{
+    File.Create(path + @"\\Data\\jsonData.json");
+}
+
+
+string jsonData = @"C:\\Users\\alita\\source\\repos\\TaskFileDirectorySerializer\\Data\\jsonData.json";
+string url = @"https://jsonplaceholder.typicode.com/posts";
+HttpClient httpClient = new HttpClient();
+string result = await httpClient.GetStringAsync(url);
+string jsonSerialized = JsonConvert.SerializeObject(result);
+
+
+using (StreamWriter sw = new(jsonData))
+{
+    sw.WriteLine(jsonSerialized);
+}
